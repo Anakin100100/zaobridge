bcce5389f3e3e5745c96da78db37ac7c994a3c60
Paweł Biegun <biegunpawel900@gmail.com>
2022-03-10 20:53:47 +0100

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    def after_sign_in_path_for(resource)
+        "/"
+    end
+
+    def after_sign_up_path_for(resource)
+        "/landing"
+    end
+       
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, except: [:landing]
+    def index 
+
+    end
+
+    def landing
+
+    end
 end
