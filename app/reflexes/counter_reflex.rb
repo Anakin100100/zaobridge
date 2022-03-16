b7c27ac3482a8f0caa146a0288cb71d2ad2763fc
Paweł Biegun <biegunpawel900@gmail.com>
2022-03-16 07:38:17 +0100

    save

--- app/reflexes/counter_reflex.rb
+++ app/reflexes/counter_reflex.rb
@@ -2,4 +2,20 @@ class CounterReflex < ApplicationReflex
   def increment
     @count = element.dataset[:count].to_i + element.dataset[:step].to_i
   end
+
+  def render_1
+    morph "#main_content", render(partial: "game_sets/partial1")
+  end
+
+  def render_2
+    morph "#main_content", render(partial: "game_sets/partial2")
+  end
+
+  def render_3
+    morph "#main_content", render(partial: "game_sets/partial3")
+  end
+
+  def render_4
+    morph "#main_content", render(partial: "game_sets/partial4")
+  end
 end
